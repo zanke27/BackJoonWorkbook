9bd5f44d2fe9df22fa355f7ae0f0248ecd43f821
zanke27 <zanke27@gmail.com>
2022-09-15 12:32:48 +0900

    미로탐색 완료, 프린터 큐 진행중

--- Baekjoon/1966번 프린터 큐.cpp
+++ Baekjoon/1966번 프린터 큐.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <queue>
 using namespace std;
 
 int main(void)
@@ -7,20 +6,21 @@ int main(void)
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
-	queue<int> queue;
-
-	int testNum = 0, docNum = 0;
+	int queue[100] = {0,};
+	int docNum = 0, myNum = 0, testNum = 0;
 	int posNum = 0, tempNum = 0;
-	int count = 0;
-	int myNum = 0;
-	cin >> docNum;
-	cin >> posNum;
+	int maxNum = 0, minNum = 0;
+	
+	cin >> docNum >> posNum;
 
 	for (int i = 0; i < docNum; i++)
 	{
 		cin >> tempNum;
-		queue.push(tempNum);
+		queue[i] = tempNum;
 		if (i == posNum) myNum = tempNum;
+		if (maxNum < tempNum) maxNum = tempNum;
+		if (minNum > tempNum) minNum = tempNum;
+
 	}
 
 
